60a674a3933962a2cc0c2a54ab585dac240dad9c
Simon-Benezet <simon.benezet@hotmail.fr>
2018-12-05 17:37:59 +0100

    a

--- src/main/java/dev/top/entities/Collegue.java
+++ src/main/java/dev/top/entities/Collegue.java
@@ -14,17 +14,28 @@ public class Collegue {
 	private String photo;
 	private String pseudo;
 	private int score;
-
+	private String nom;
+	private String Prenom;
+	private String Email;
+	private String Adresse;
+	
+	
 	public Collegue() {
 
 	}
 
-	public Collegue(String photo, String pseudo, int score) {
+	public Collegue(String photo, String pseudo, int score, String nom, String prenom, String email, String adresse) {
 		super();
 		this.photo = photo;
 		this.pseudo = pseudo;
 		this.score = score;
+		this.nom = nom;
+		Prenom = prenom;
+		Email = email;
+		Adresse = adresse;
 	}
+
+
 
 	public Integer getId() {
 		return id;
@@ -56,6 +67,38 @@ public class Collegue {
 
 	public void setPseudo(String pseudo) {
 		this.pseudo = pseudo;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return Prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		Prenom = prenom;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public String getAdresse() {
+		return Adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		Adresse = adresse;
 	}
 
 }
