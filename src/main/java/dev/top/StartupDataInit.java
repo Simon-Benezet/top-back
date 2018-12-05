83130f4f1592eb0233ad28ba4ca455d37d5cca42
Simon-Benezet <simon.benezet@hotmail.fr>
2018-12-05 11:35:43 +0100

    a

--- src/main/java/dev/top/StartupDataInit.java
+++ src/main/java/dev/top/StartupDataInit.java
@@ -22,12 +22,13 @@ public class StartupDataInit {
 	public void init() {
 
 		if (this.versionRepo.count() <= 0) {
-//			this.versionRepo.save(new Version("v1", 1));
-//			this.versionRepo.save(new Version("v2", 2));
-//			this.versionRepo.save(new Version("v3", 3));
-//			this.versionRepo.save(new Version("v4", 4));
+			this.versionRepo.save(new Version("v1", 1));
+			this.versionRepo.save(new Version("v2", 2));
+			this.versionRepo.save(new Version("v3", 3));
+			this.versionRepo.save(new Version("v4", 4));
 		}
 
+		if (this.collegueRepo.count() <= 0) {
 			this.collegueRepo.save(new Collegue(
 					"https://images.pexels.com/photos/265036/pexels-photo-265036.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb",
 					"Rod", 0));
@@ -37,6 +38,6 @@ public class StartupDataInit {
 			this.collegueRepo.save(new Collegue(
 					"https://cdn-images-1.medium.com/max/1200/0*pAdZLfSqNrMZAAPA.jpg",
 					"Didier", 0));
-		
+		}
 	}
 }
